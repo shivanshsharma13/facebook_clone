e4f9f6b0b757f0a3cd02dde8c43644652809199c
shivansh sharma <shivansh.sharma706@gmail.com>
2020-10-25 12:13:23 +0530

    Updates

---
+++ precache-manifest.4f48fbe62c1cf25935a300c8e2e320b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41e5af74f303f1c3aea43d280921d5e",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "72b33e124c2a7fd29636",
+    "url": "/facebook-clone/static/css/main.86823631.chunk.css"
+  },
+  {
+    "revision": "fceecd29aa0ea8b2639b",
+    "url": "/facebook-clone/static/js/2.99ce5d59.chunk.js"
+  },
+  {
+    "revision": "bd5c8e8fa25e90f36b464e8c3611da37",
+    "url": "/facebook-clone/static/js/2.99ce5d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b33e124c2a7fd29636",
+    "url": "/facebook-clone/static/js/main.1f35b04e.chunk.js"
+  },
+  {
+    "revision": "33761bbefdcaf2fb5a11",
+    "url": "/facebook-clone/static/js/runtime-main.7a86f86c.js"
+  }
+]);
