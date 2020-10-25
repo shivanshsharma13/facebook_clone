99ac0b017f52d12fcb41823aaab21a043c8a1360
shivkaansh <shivansh.sharma706@gmail.com>
2020-10-25 23:58:33 +0530

    updated the responsivness

--- src/Widgets.js
+++ src/Widgets.js
@@ -5,7 +5,7 @@ function Widget() {
         <div className="Widgets">
             <iframe
             src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAnimal-Love-521936607981789&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-            width="340"
+            width="100%"
             height="100%"
             style={{border: "none", overflow:"hidden"}}
             scrolling="no"
